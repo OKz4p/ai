08ac0a40e9ee8788317e0f7b40126b107a1433ab
OKz4p <daniela721@naver.com>
2026-08-04 18:01:45 +0900

    자바스크립트

---
+++ source/03_javaScript/ch02_기본문법/2_변수선언.js
@@ -0,0 +1,18 @@
+// 자료형 : string, number, boolean, function, object(array), undefined, null
+// undefined는 변수 초기화가 안되었을 때, null은 개발자가 인위적으로 null을 할당
+var variable;
+console.log("1.variable 타입 :", typeof(variable), "-값:", variable);
+//variable = "이름은 '홍길똥강아지' 입니다" ;
+let name = "홍길동강아지";
+variable = `이름은 "${name}"입니다`;
+console.log("2.variable 타입 :", typeof(variable), "-값:", variable);
+variable = -3.2323;
+console.log("3.variable 타입 :", typeof(variable), "-값:", variable);
+variable = false;
+console.log("4.variable 타입 :", typeof(variable), "-값:", variable);
+variable = function(){
+    alert("hello");
+};
+console.log("5.variable 타입 :", typeof(variable), "-값:", variable);
+variable = null; // null은 타입이 Object
+console.log("6.variable 타입 :", typeof(variable), "-값:", variable);
